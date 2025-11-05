889cc419737c104653e1c997b95f1591fec533cb
Rashif Dwi <rashif368@gmail.com>
2025-11-05 14:05:47 +0700

    tambah asset

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,24 @@
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Mainpage from "./pages/mainpage/home";
+import Login from "./pages/mainpage/formlogin";
+import bg from "./assets/pnm_background.jpeg";
 
-import './App.css';
-import { Mainpage } from './pages/mainpage/home';
-import { Routes, Route } from 'react-router-dom';
 function App() {
-
   return (
-    <Routes>
-      <Route path="/" element={<Mainpage />} />
-    </Routes>
+    <div
+      className="min-h-screen bg-cover bg-center"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
+      <Routes>
+    
+        <Route path="/formlogin" element={<Login />} />
+        <Route path="/" element={<Mainpage />} />
+
+
+      </Routes>
+    </div>
   );
 }
 
