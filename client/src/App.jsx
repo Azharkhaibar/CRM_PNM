6f524201c0c431c428b047b96e3aa4da08a5606f
ASUS <azharasykari@gmail.com>
2025-11-05 14:55:47 +0700

    adding handle error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,12 @@
-import React from "react";
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import Mainpage from "./pages/mainpage/home";
-
+import React from 'react';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Mainpage from './pages/mainpage/home';
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Mainpage />} />
-
-
       </Routes>
     </div>
   );
--- client/src/pages/mainpage/home.jsx
+++ client/src/pages/mainpage/home.jsx
@@ -1,8 +1,10 @@
-export const Mainpage = () => {
-    return (
-        <div>
-            <p>testing homepage</p>
-            <h2>tastes</h2>
-        </div>
-    )
-}
+const Mainpage = () => {
+  return (
+    <div>
+      <p  className="text-yellow-700 text-2xl">testing homepage</p>
+      <h2>tastes</h2>
+    </div>
+  );
+};
+
+export default Mainpage;
