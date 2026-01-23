928f8de4104cfd5f298091c2470dde781602b24c
Rashif <rashif368@gmail.com>
2026-01-23 11:01:29 +0700

    chore: configure shadcn alias and vite setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,23 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import tailwindcss from '@tailwindcss/vite';
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
   server: {
     port: 5173,
     proxy: {
-      '/api': {
-        target: 'http://localhost:5530', // backend NestJS kamu
-        changeOrigin: true, // penting biar host header diubah
-        secure: false, // nonaktifkan SSL check (kalau http)
+      "/api": {
+        target: "http://localhost:5530",
+        changeOrigin: true,
+        secure: false,
       },
     },
   },
